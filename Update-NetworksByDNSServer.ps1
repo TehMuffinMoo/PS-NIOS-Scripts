ece7fe5a5806cd1f6efeaeeba36519cf0df532b9
TehMuffinMoo <mat.cox@tmmn.uk>
2024-04-25 16:24:05 +0100

    Later version

--- Update-NetworksByDNSServer.ps1
+++ Update-NetworksByDNSServer.ps1
@@ -6,7 +6,7 @@ function Get-AllNIOSNetworks {
     $Count += $Network.result.Count
     Write-Host "Queried $($Count) network objects"
     $NetworkResults += $Network.result
-    
+   
     while ($Network.next_page_id -ne $null) {
         $Network = Get-IBObject -ObjectRef "network?_max_results=$($Limit)&_paging=1&_return_as_object=1&_page_id=$($Network.next_page_id)&_return_fields%2b=options"
         $Count += $Network.result.Count
@@ -15,7 +15,7 @@ function Get-AllNIOSNetworks {
     }
     return $NetworkResults
 }
-
+ 
 function Find-NIOSNetworksWithOldDNS {
     param (
         [System.Object]$Networks,
@@ -33,7 +33,7 @@ function Find-NIOSNetworksWithOldDNS {
     }
     return $NetworksToUpdate
 }
-
+ 
 function Set-NIOSNetworksDNS {
     param (
         [PSCustomObject]$Networks,
@@ -43,7 +43,7 @@ function Set-NIOSNetworksDNS {
     )
     $Objects = $Networks | ConvertTo-Json -Depth 5 | ConvertFrom-Json -Depth 5
     Write-Host "Objects to Update:"
-    $Objects | Select-Object network,network_view,@{name='dns_servers';expr={($_.options | Where-Object {$_.name -eq "domain-name-servers"}).value -join ","}}
+    $Objects | Select-Object network,network_view,@{name='dns_servers';expr={($_.options | Where-Object {$_.name -eq "domain-name-servers"}).value -join ","}} | ft -AutoSize
     foreach ($NWWOD in $Objects) {
         [String[]]$DNSServers = $null
         $DNSServers = ($NWWOD.options | Where-Object {$_.name -eq 'domain-name-servers'}).value -split ','
@@ -56,21 +56,29 @@ function Set-NIOSNetworksDNS {
         ($NWWOD.options | Where-Object {$_.name -eq 'domain-name-servers'}).value = ($DNSServers -join ',')
     }
     Write-Host "Changes to apply:"
-    $Objects | Select-Object network,network_view,@{name='dns_servers';expr={($_.options | Where-Object {$_.name -eq "domain-name-servers"}).value -join ","}}
+    $Objects | Select-Object network,network_view,@{name='dns_servers';expr={($_.options | Where-Object {$_.name -eq "domain-name-servers"}).value -join ","}} | ft -AutoSize
     if (!($DryRun)) {
         Write-Host "Applying changes to network objects.."
-        $Objects | Select-Object -ExcludeProperty network_view | Set-IBObject
+        $Updates = @()
+        foreach ($O in $Objects) {
+            $Updates += @{
+                "_ref" = $O._ref
+                "network" = $O.network
+                "options" = $O.options
+            }
+        }
+        $Updates | Set-IBObject
     }
 }
-
+ 
 ## Get list of all NIOS Networks
 $AllNetworks = Get-AllNIOSNetworks
-
+ 
 ## "IF Values" - Populate whole list of IPs here
 $OldIPs = '10.123.123.10','10.123.123.20'
-
+ 
 ## Identify list of networks with old DNS server(s)
 $NetworksWithOldDNS = Find-NIOSNetworksWithOldDNS $AllNetworks $OldIPs
-
+ 
 ## Replace Old DNS IPs with New on each network. Remove -DryRun to implement the changes
-Set-NIOSNetworksDNS $NetworksWithOldDNS $OldIPs '10.255.255.1' -DryRun
+Set-NIOSNetworksDNS $NetworksWithOldDNS $OldIPs '10.255.255.1','10.255.255.2' -DryRun
